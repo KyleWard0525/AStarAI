28f6f3684bcc4cecaacec989bb9110aeb9aa0914
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-10-07 19:09:44 -0400

    Driver code

---
+++ src/astarai/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package astarai;
+
+import astarai.ui.GameWindow;
+
+/**
+ *
+ * Main program driver
+ * 
+ * @author kward60
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameWindow gw = new GameWindow();
+        gw.setVisible(true);
+    }
+    
+}
