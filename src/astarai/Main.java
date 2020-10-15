ecb4887eb8e67528f8789566eec4dcdc9c93a142
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-10-15 18:46:19 -0400

    Added Game Engine

--- src/astarai/Main.java
+++ src/astarai/Main.java
@@ -21,6 +21,7 @@ public class Main {
     public static void main(String[] args) {
         GameWindow gw = new GameWindow();
         gw.setVisible(true);
+        GameEngine engine = new GameEngine(gw, 15, 15);
     }
     
 }
